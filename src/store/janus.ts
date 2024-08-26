2b61f900e9a1b8015e7418700b2f1202df79004c
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-08-26 13:15:08 +0800

    first commit

--- src/store/janus.ts
+++ src/store/janus.ts
@@ -152,7 +152,7 @@ class JanusStore {
       dependencies: Janus.useDefaultDependencies({ adapter }),
       callback: () => {
         this.janus = new Janus({
-          server: ["wss://janus-gateway.koreansimple.com:8188/", "https://janus-gateway.koreansimple.com/janus"],
+          server: ["wss://janus-gateway.koreansimple.com:8189/", "https://janus-gateway.koreansimple.com/janus"],
           success: () => {
             this.attach(callback);
           },
